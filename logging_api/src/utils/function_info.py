8fb4eef3bb022f9da5a57ee20b6b0f5313b53082
Romero Torres <jcromerot@indracompany.com>
2022-11-18 23:40:36 -0500

    feature: add new way to get offset line

--- logging_api/src/utils/function_info.py
+++ logging_api/src/utils/function_info.py
@@ -1,12 +1,22 @@
 import inspect
 import os
+from typing import List
+
+def get_offset_line(keyword: str, arr_strings: List) -> int:
+    ans = 0
+    for string_elemen in arr_strings:
+        if keyword in string_elemen:
+            return ans
+        ans = ans + 1  
+    return ans
 
 def get_path_file(func):
 
     module_address = func.__globals__['__file__']
-    line_addres = inspect.getsourcelines(func)[1]
+    info_function = inspect.getsourcelines(func) 
+    line_addres = info_function[1]
     function_name = func.__name__
 
-    function_path = "%s:%d" % (module_address, line_addres+1)
+    function_path = "%s:%d" % (module_address, line_addres+get_offset_line( function_name,  info_function[0]))
 
     return [function_name, function_path]
